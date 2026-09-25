e032c01d5003aa54a5e14512dee7c323b8d28b92
acohon <showwinho@gmail.com>
2026-09-25 12:53:27 +0800

    star with React JS function

---
+++ Javascript/full examples/script16.js
@@ -0,0 +1,54 @@
+// input element
+const itemInput = document.getElementById("item-input");
+const priorityInput = document.getElementById("priority-input");
+const checkbox = document.getElementById("checkbox");
+const heading = document.querySelector("h1");
+
+function onInput(e) {
+    heading.textContent = e.target.value;
+}
+itemInput.addEventListener("input", onInput);
+priorityInput.addEventListener("change", onInput);
+
+function onChecked(e) {
+    const isChecked = e.target.checked;
+    heading.textContent = isChecked ? "checked" : "unchecked";
+}
+checkbox.addEventListener("input", onChecked);
+
+function onFocus() {
+    itemInput.style.outlineStyle = "solid";
+    itemInput.style.outlineColor = "red";
+    itemInput.style.outlineWild = "2px";
+}
+
+function onBlur() {
+    itemInput.style.outlineStyle = "none";
+}
+    itemInput.addEventListener("focus", onFocus);
+    itemInput.addEventListener("blur", onBlur);
+
+const form = document.getElementById("item-form");
+function onSubmit(e) {
+    const item = document.getElementById("item-input").value;
+    const priority = document.getElementById("priority-input").value;
+    if(item === "" || priority === "0") {
+        alert("please fill in fields");
+    }
+    e.preventDefault();
+}
+form.addEventListener("submit", onSubmit2);
+
+
+// react to NEXT js common function
+function onSubmit2(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const item = formData.get("item");
+    const priority = formData.get("priority");
+    const entries = formData.entries();
+    console.log(entries);
+    for (let entry of entries) {
+        console.log(entry[1]);
+    }
+}
